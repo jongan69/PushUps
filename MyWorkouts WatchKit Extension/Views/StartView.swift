00d33fae85b46f0567085d4b2649909b0e541547
jongan69 <jon.gan@kinandcarta.com>
2023-01-07 18:30:46 -0500

    initial commit '

---
+++ MyWorkouts WatchKit Extension/Views/StartView.swift
@@ -0,0 +1,48 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The start view.
+*/
+
+import SwiftUI
+import HealthKit
+
+struct StartView: View {
+    @EnvironmentObject var workoutManager: WorkoutManager
+    var workoutTypes: [HKWorkoutActivityType] = [.functionalStrengthTraining]
+
+    var body: some View {
+        List(workoutTypes) { workoutType in
+            NavigationLink(workoutType.name, destination: SessionPagingView(),
+                           tag: workoutType, selection: $workoutManager.selectedWorkout)
+                .padding(EdgeInsets(top: 15, leading: 5, bottom: 15, trailing: 5))
+        }
+        .listStyle(.carousel)
+        .navigationBarTitle("Workouts")
+        .onAppear {
+            workoutManager.requestAuthorization()
+        }
+    }
+}
+
+struct StartView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartView().environmentObject(WorkoutManager())
+    }
+}
+
+extension HKWorkoutActivityType: Identifiable {
+    public var id: UInt {
+        rawValue
+    }
+
+    var name: String {
+        switch self {
+        case .functionalStrengthTraining:
+            return "Push Ups"
+        default:
+            return ""
+        }
+    }
+}
